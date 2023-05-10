b1f626bbfc6b9564dc399426c96be8d50686081f
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2023-05-10 12:51:41 +0200

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,8 @@
+# This file has been written to your home directory for convenience. It is
+# saved as "/home/joseph/temperature-2023-05-10-12-12-27.py"
+
+from sense_emu import SenseHat
+
+sense = SenseHat()
+
+sense.show_message("Hello World")
