ca22d9063c167ae7fa44da86a8281b08043a3673
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2023-05-10 17:42:51 +0200

    Update Binary_Clock.py

--- Binary_Clock.py
+++ Binary_Clock.py
@@ -19,3 +19,40 @@ while True:
         sense.set_pixel(5,i, int(second_bin[i]) * 255, 255, 255)
         
     sleep(1)
+    
+    
+    #-------------------------- changes ------------------------------------------
+    
+from sense_emu import SenseHat
+from time import sleep
+import datetime
+
+sense = SenseHat()
+
+def get_time():
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    return hour, minute, second
+
+def get_binary_digits(value, num_bits):
+    binary_digits = []
+    for i in range(num_bits):
+        binary_digits.append(value % 2)
+        value //= 2
+    return binary_digits
+
+def display_time(hour, minute, second):
+    hour_digits = get_binary_digits(hour, 6)
+    minute_digits = get_binary_digits(minute, 6)
+    second_digits = get_binary_digits(second, 6)
+    for i in range(6):
+        sense.set_pixel(1, i+1, hour_digits[i]*255, hour_digits[i]*255, hour_digits[i]*255)
+        sense.set_pixel(3, i+1, minute_digits[i]*255, minute_digits[i]*255, minute_digits[i]*255)
+        sense.set_pixel(5, i+1, second_digits[i]*255, second_digits[i]*255, second_digits[i]*255)
+
+while True:
+    hour, minute, second = get_time()
+    display_time(hour, minute, second)
+    sleep(1)
